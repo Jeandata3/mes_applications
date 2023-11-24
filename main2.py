06e12e0715355c2ebae8a7bccb1befdff4c79a86
Jeandata3 <138308844+Jeandata3@users.noreply.github.com>
2023-11-24 14:26:46 +0000

    Update main2.py

--- main2.py
+++ main2.py
@@ -10,14 +10,7 @@ import seaborn as sns
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold,GroupKFold
-from sklearn import svm
-from sklearn.svm import SVC
 import lazypredict
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
